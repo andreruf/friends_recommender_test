89a75950efa00ffe29a72f2edbac8787c54f201c
andreruf <andre_rufino10@hotmail.com>
2024-01-14 14:19:54 -0300

    feat: add create person domain usecase

---
+++ src/domain/models/person.ts
@@ -0,0 +1,5 @@
+export interface PersonModel {
+  name: string
+  cpf: string
+  friends?: string[]
+}
---
+++ src/domain/usecases/create-person.ts
@@ -0,0 +1,10 @@
+import { type PersonModel } from '../models'
+
+export interface CreatePersonModel {
+  name: string
+  cpf: string
+}
+
+export interface CreatePerson {
+  create: (person: CreatePersonModel) => Promise<PersonModel>
+}
