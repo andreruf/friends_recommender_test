4f401759aa0035ed81f81382b563faed85e7457f
andreruf <andre_rufino10@hotmail.com>
2024-01-14 14:07:39 -0300

    chore: adjust jest and lint config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/domain/**', '!<rootDir>/src/data/models/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.*\\.ts$': 'ts-jest'
