a23864af1b20cd9300c2cea04a03432606dee331
andreruf <andre_rufino10@hotmail.com>
2024-01-14 14:29:47 -0300

    feat: add express route adapter

---
+++ src/main/adapters/express/express-route-adapter.ts
@@ -0,0 +1,14 @@
+import { type Controller, type HttpRequest } from '../../../presentation/protocols'
+import { type Request, type Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      params: req.params
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
