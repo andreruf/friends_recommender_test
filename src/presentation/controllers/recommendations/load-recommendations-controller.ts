aa350627af6f3753ec073101a7f6d83dbfa1ea3a
andreruf <andre_rufino10@hotmail.com>
2024-01-15 01:58:06 -0300

    feat: add load recommendations controller

---
+++ src/presentation/controllers/recommendations/load-recommendations-controller.ts
@@ -0,0 +1,46 @@
+import { type HttpResponse, type Controller, type CpfValidator } from './load-recommendations-protocols'
+import { InvalidParamError, NotFoundError } from '../../errors'
+import { badRequest, notFound, ok, serverError } from '../../helpers/http-helper'
+import { type LoadPerson } from '../../../domain/usecases/load-person'
+import { type LoadRecommendations } from '../../../domain/usecases/load-recommendations'
+
+export class LoadRecommendationsController implements Controller {
+  constructor (
+    private readonly cpfValidator: CpfValidator,
+    private readonly loadRecommendations: LoadRecommendations,
+    private readonly loadPerson: LoadPerson
+  ) {}
+
+  async handle (httpRequest: LoadRecommendationsController.Request): Promise<HttpResponse> {
+    try {
+      const { cpf } = httpRequest.params
+
+      const isValid = this.cpfValidator.isValid(cpf)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('cpf'))
+      }
+
+      const cpfExist = await this.loadPerson.load(cpf)
+
+      if (!cpfExist) {
+        return notFound(new NotFoundError('cpf'))
+      }
+
+      const response = await this.loadRecommendations.load(cpf)
+
+      return ok(response)
+    } catch (error) {
+      console.log(error)
+      return serverError()
+    }
+  }
+}
+
+export namespace LoadRecommendationsController {
+  export type Request = {
+    params: {
+      cpf: string
+    }
+  }
+}
---
+++ src/presentation/controllers/recommendations/load-recommendations-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../protocols'
+export * from '../../../domain/usecases/load-person'
+export * from '../../../domain/models/person'
