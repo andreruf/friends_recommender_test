b03f41d3e0ae6b89938650e7a536c7bf85e0ec69
andreruf <andre_rufino10@hotmail.com>
2024-01-20 02:00:30 -0300

    fix: remove load person dependency from controller and use usecase response

--- src/main/factories/controllers/load-recommendations-controller-factory.ts
+++ src/main/factories/controllers/load-recommendations-controller-factory.ts
@@ -1,10 +1,9 @@
 import { LoadRecommendationsController } from '../../../presentation/controllers/recommendations/load-recommendations-controller'
 import { CpfValidatorAdapter } from '../../../validation/validators/cpf-validator-adapter'
 import { makeLoadRecommendations } from '../usecases/load-recommendations-factory'
-import { makeLoadPerson } from '../usecases/load-person-factory'
 
 export const makeLoadRecommendationsController = (): LoadRecommendationsController => {
   const cpfValidatorAdapter = new CpfValidatorAdapter()
 
-  return new LoadRecommendationsController(cpfValidatorAdapter, makeLoadRecommendations(), makeLoadPerson())
+  return new LoadRecommendationsController(cpfValidatorAdapter, makeLoadRecommendations())
 }
