bca977f2e51ab1161fb98f0b1aa29078defe8530
andreruf <andre_rufino10@hotmail.com>
2024-01-20 01:30:16 -0300

    refactor: adjust load person usecase layers responsibilities and performance

--- src/infra/in-memory-db/in-memory-load-person.ts
+++ src/infra/in-memory-db/in-memory-load-person.ts
@@ -1,7 +1,7 @@
 import { type LoadPersonRepository, type PersonModel } from '../../data/usecases/load-person/load-person-protocols'
 
 export class InMemoryLoadPersonRepository implements LoadPersonRepository {
-  async load (cpf: string): Promise<PersonModel> {
+  async load (cpf: string): Promise<PersonModel | null> {
     const personCollection = global.personCollection || []
 
     const personByCpf = personCollection.find((person) => person.cpf === cpf)
--- src/presentation/controllers/load-person/load-person-protocols.ts
+++ src/presentation/controllers/load-person/load-person-protocols.ts
@@ -1,4 +1,3 @@
 export * from '../../protocols'
-export * from '../../protocols/cpf-validator'
 export * from '../../../domain/usecases/load-person'
 export * from '../../../domain/models/person'
